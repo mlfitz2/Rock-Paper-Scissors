eed1694c82266539584f81b1aef3289213a4fb6e
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-01-04 21:26:57 -0800

    fully functional

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('test')
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var options = ['R', 'P', 'S'];
+
+function playGame() {
+    var userChoice = window.prompt('Enter R, P, or S');
+    userChoice = userChoice.toUpperCase();
+
+    var i = Math.floor(Math.random() * options.length);
+    var computerChoice = options[i];
+    window.alert("The computer chose " + computerChoice + "!")
+
+    if (computerChoice === userChoice) {
+        window.alert("It's a tie!")
+        ties++;
+    } else if (
+        (userChoice === 'R' && computerChoice === 'S') ||
+        (userChoice === 'P' && computerChoice === 'R') ||
+        (userChoice === 'S' && computerChoice === 'P')
+    ) {
+        window.alert("You win!")
+        wins++;
+    } else {
+        window.alert("You lose!")
+        losses++;
+    }
+
+    window.alert("Scoreboard:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
+
+    var playAgain = window.confirm("One more round?");
+    if (playAgain) {
+        playGame()
+    } 
+
+
+
+
+}
+
+
+
+
+
+playGame()
